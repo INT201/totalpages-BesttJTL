f132b52caa8566db9450cfdfbac13335bc7766de
BesttJTL <111588284+BesttJTL@users.noreply.github.com>
2022-09-20 15:56:32 +0700

    Bye

--- totalPages.js
+++ totalPages.js
@@ -1,6 +1,17 @@
 const { template } = require('@babel/core')
 
 function totalPages(arrayItems, rowsPerPage) {
-  //code here
+  if (arrayItems==null || arrayItems==undefined ){
+    return undefined
+  }
+  else if (rowsPerPage==false || rowsPerPage==null || rowsPerPage==undefined){
+    return 1 
+  }
+  else {
+    let y = arrayItems.length
+    let x = y/rowsPerPage
+    let a = Math.ceil(x)
+    return a
+  }
 }
 module.exports = totalPages
